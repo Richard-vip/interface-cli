1a23eb689e9471a258f1e97ec47d2d71c3052176
Richard-vip <18765330693@163.com>
2023-04-19 00:32:36 +0800

    feat: unit test

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,40 @@
 module.exports = {
   testMatch: ['**/?(*.)(spec|test).ts?(x)'],
-  rootDir: '',
   transform: {
     '.(ts|tsx)': '@swc/jest',
   },
-  
+  coverageThreshold: {
+    global: {
+      branches: 50,
+      functions: 95,
+      lines: 95,
+      statements: 95,
+    },
+  },
+  collectCoverage: true,
+  coverageDirectory: './docs/jest-coverage',
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx,ts,tsx}',
+    '!**/node_modules/**',
+    '!**/vendor/**',
+  ],
+  coveragePathIgnorePatterns: ['/node_modules/', '/tests/'],
+  //测试文件的类型
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'json', 'jsx', 'node'],
+  reporters: [
+    "default",
+    [
+      "jest-stare",
+      {
+        "resultDir": "../../docs/jest-stare",
+        "reportTitle": "jest-stare!",
+        "additionalResultsProcessors": [
+          "jest-junit"
+        ],
+        "coverageLink": "../jest-coverage/lcov-report/index.html",
+        "jestStareConfigJson": "jest-stare.json",
+        "jestGlobalConfigJson": "globalStuff.json"
+      }
+    ]
+  ],
 }
--- tests/unit/index.spec.ts
+++ tests/unit/index.spec.ts
@@ -1,6 +1,6 @@
-describe('Name of the group', () => {
-  it('should ', async () => {
-    
+describe('综合性的测试', () => {
+  it('简单的测试', () => {
+    expect(11).toBe(11);
   });
 });
 
